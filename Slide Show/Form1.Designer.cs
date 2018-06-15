c4ad4ed2282689975903d888981c00fbb6683873
Ian Pendergast <ianpendergast@yahoo.com>
2018-06-15 14:22:01 +1000

    Working - Ugly UI

--- Slide Show/Form1.Designer.cs
+++ Slide Show/Form1.Designer.cs
@@ -160,6 +160,9 @@
             this.Controls.Add(this.StartSlideShowBtn);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
             this.Name = "Form1";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Slide Show";
